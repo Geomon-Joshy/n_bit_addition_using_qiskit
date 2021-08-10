2ec40f49b30618e8db3ed2a301e340120a2f505e
Geomon Joshy <geomonjoshy@gmail.com>
2021-08-10 18:53:32 +0530

    Update n_bit_addition.py

--- n_bit_addition.py
+++ n_bit_addition.py
@@ -4,21 +4,33 @@ from numpy import array
 sim=Aer.get_backend('aer_simulator')
 q=0
 def conv(a,n): # defining a fuction to covert a decimal number into binary codes
-  r=array([0,0,0,0,0])
-  if a==1: #if the decimal numbe is one the fisrt value in array is made a 1
-    r[0]=1
-  for i in range(a): #the values in array is made one when the decimal number gives reminder on undergoin continues division by 2 
-    if a>=1:
-      r[i]=a%2
-      a=a/2
-    i=i+1
-  return r
-def sum_circuit(qc,i): #defining a function to add two bits and the carry from previous addition
-  qc.ccx(i,i+1,i+3)
-  qc.cx(i,i+1)
-  qc.ccx(i+1,i+2,i+3)
-  qc.cx(i+1,i+2)
-  return qc
+    """ Args:  
+        (int): the number to be converted.
+        n (int): the number of bits.
+    Returns:
+        r(array) An array which returns the binary value of the integer.
+  """       
+    r=array([0]*n)
+    if a==1: #if the decimal numbe is one the fisrt value in array is made a 1
+        r[0]=1
+    for i in range(a): #the values in array is made one when the decimal number gives reminder on undergoin continues division by 2 
+        if a>=1:
+              r[i]=a%2
+              a=a/2
+        i=i+1
+    return r
+def sum_circuit(qc,i): #defining a function to add two bits and the carry from previous addition.
+    """ Args:
+        qc(QuantumCircuit) : the circuit which has to has to undergo addition.
+        i (int): the postion of the pervious addition's carry.
+    Returns:
+        qc(QuantumCircuit) it gives the new circuit after the changes have been made.
+  """
+    qc.ccx(i,i+1,i+3)
+    qc.cx(i,i+1)
+    qc.ccx(i+1,i+2,i+3)
+    qc.cx(i+1,i+2)
+    return qc
 n= int(input("Enter number of bits used:")) #initializing number of bits to be used
 a= int(input("Enter the first number:")) #initializing first input number
 b= int(input("Enter the second number:")) #initializing second input number
@@ -51,3 +63,5 @@ qc.measure((3*n)-1,n) #measuring the carry of highest value addition
 qobj = assemble(qc) #simulating the circuit
 counts = sim.run(qobj).result().get_counts()
 plot_histogram(counts)  #plotting histogram
+s=list(counts.keys())[0] # trasfering the result into a sting s
+print("Answer is :"int(s, 2)) #printing the decimal value
